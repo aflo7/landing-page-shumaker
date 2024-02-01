cb715b51c3c618069b782e04b16b717e9238f7bc
Andres Flores <andresflo883@gmail.com>
2024-01-31 23:26:27 -0500

    updated the readme and added drawer component

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This is a remake of the Shumaker Landing Page. I used tech like React, Javascript, HTML, CSS and Vite for this project.
 
+I also used Material Design for this project to get the right sidebar.
+
 I created a deploy file that deploys the application on every git push.
 
 See the site here: https://aflo7.github.io/landing-page-shumaker/
@@ -8,6 +10,11 @@ Mobile view
 
 ![alt](./mobile.png)
 
+The mobile version does have a drawer, see a screenshot below
+
+![alt](./thedrawer.png)
+
 Desktop view
 
 ![alt](./desktop.png)
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,145 @@
 import './App.css';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { FaSearch } from 'react-icons/fa';
+import { useState } from 'react';
+import Drawer from '@mui/material/Drawer';
+import { IoClose } from 'react-icons/io5';
 
 export default function App() {
+  const [opened, setOpened] = useState(false);
+
   return (
     <>
-      <div className="nav-wrapper">
-        <nav>
-          <h1>Logo here</h1>
-
-          <div className="desktop-right-nav">
-            <div>Mock</div>
-            <div>Mock</div>
-            <div>Mock</div>
-            <div>Mock</div>
-            <div>Mock</div>
-            <div>Mock</div>
+      <>
+        <Drawer anchor="right" open={opened} onClose={() => setOpened(false)}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              padding: '10px'
+            }}
+          >
+            <IoClose
+              className="io-close-btn"
+              size="1.5rem"
+              color="gray"
+              onClick={() => setOpened(false)}
+            />
           </div>
-
-          <GiHamburgerMenu className="gi-hamburger-menu" size="1.5rem" />
-        </nav>
-        <div className="contact-us-wrapper">
-          <div className="contact-us-wrapper-inner">
-            <button className="contact-us-btn">Contact Us</button>
-            <FaSearch />
+          <div className="drawer-wrapper" style={{ padding: '0px 40px' }}>
+            <div>Home</div>
+            <div>Quote</div>
+            <div>Solutions</div>
+            <div>Portfolio</div>
+            <div>Support</div>
+            <div>About</div>
           </div>
-        </div>
-        <div className="career-opportunities-wrapper">
-          <div className="career-opportunities">Career Opportunities</div>
-        </div>
-      </div>
+        </Drawer>
 
-      <div className="h2-header-wrapper-outer">
-        <div className="h2-header-wrapper">
-          <div>
-            <div>
-              <h2 className="custom-web-header">
-                Made By Andres Flores, using React
-              </h2>
-              <p>
-                This page is responsive for mobile and desktop devices. It's a
-                mock page made to show off my development skills. Lorem, ipsum
-                dolor sit amet consectetur adipisicing elit. Ex natus iusto
-                voluptate blanditiis? Earum, aut consequatur iusto voluptas
-                maxime doloribus?
-              </p>
+        <div className="nav-wrapper">
+          <nav>
+            <h1>Logo here</h1>
+
+            <div className="desktop-right-nav">
+              <div>Mock</div>
+              <div>Mock</div>
+              <div>Mock</div>
+              <div>Mock</div>
+              <div>Mock</div>
+              <div>Mock</div>
             </div>
-            <button className="view-portfolio-btn">View Portfolio</button>
+            <div className="mobile-right-nav">
+              <div>Click to open the drawer &rarr;</div>
+              <GiHamburgerMenu
+                onClick={() => setOpened((prev) => !prev)}
+                className="gi-hamburger-menu"
+                size="1.5rem"
+              />
+            </div>
+          </nav>
+          <div className="contact-us-wrapper">
+            <div className="contact-us-wrapper-inner">
+              <button className="contact-us-btn">Contact Us</button>
+              <FaSearch />
+            </div>
           </div>
-          <div className="website-wrapper">
-            <div className="website-wrapper-inner">Slideshow goes here</div>
+          <div className="career-opportunities-wrapper">
+            <div className="career-opportunities">Career Opportunities</div>
           </div>
         </div>
-      </div>
 
-      <div className="our-solutions-wrapper-outer">
-        <div className="our-solutions-wrapper">
-          <div>
-            <div className="left-solution-card-wrapper">
-              <div className="card-wrapper-1">
-                <div></div>
-                <div></div>
-              </div>
-              <div className="card-wrapper-2">
-                <div></div>
-                <div></div>
-              </div>
-            </div>
-
+        <div className="h2-header-wrapper-outer">
+          <div className="h2-header-wrapper">
             <div>
-              <div
-                style={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  gap: '40px'
-                }}
-              >
+              <div>
+                <h2 className="custom-web-header">
+                  Made By Andres Flores, using React
+                </h2>
+                <p>
+                  This page is responsive for mobile and desktop devices. It's a
+                  mock page made to show off my development skills. Lorem, ipsum
+                  dolor sit amet consectetur adipisicing elit. Ex natus iusto
+                  voluptate blanditiis? Earum, aut consequatur iusto voluptas
+                  maxime doloribus?
+                </p>
+              </div>
+              <button className="view-portfolio-btn">View Portfolio</button>
+            </div>
+            <div className="website-wrapper">
+              <div className="website-wrapper-inner">Slideshow goes here</div>
+            </div>
+          </div>
+        </div>
+
+        <div className="our-solutions-wrapper-outer">
+          <div className="our-solutions-wrapper">
+            <div>
+              <div className="left-solution-card-wrapper">
+                <div className="card-wrapper-1">
+                  <div></div>
+                  <div></div>
+                </div>
+                <div className="card-wrapper-2">
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+
+              <div>
                 <div
                   style={{
                     display: 'flex',
                     flexDirection: 'column',
-                    gap: '10px'
+                    gap: '40px'
                   }}
                 >
-                  <h2 className="our-solutions-header">Our Solutions</h2>
-                  <p className="our-solutions-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Consectetur placeat, alias aliquam, ratione cupiditate
-                    maxime provident, est illo voluptates vel eos pariatur
-                    voluptatibus sed similique dignissimos obcaecati. Saepe
-                    ipsam, omnis esse delectus exercitationem cupiditate
-                    voluptas molestias, laudantium consequatur, laborum aliquid.
-                  </p>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '10px'
+                    }}
+                  >
+                    <h2 className="our-solutions-header">Our Solutions</h2>
+                    <p className="our-solutions-text">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Consectetur placeat, alias aliquam, ratione cupiditate
+                      maxime provident, est illo voluptates vel eos pariatur
+                      voluptatibus sed similique dignissimos obcaecati. Saepe
+                      ipsam, omnis esse delectus exercitationem cupiditate
+                      voluptas molestias, laudantium consequatur, laborum
+                      aliquid.
+                    </p>
+                  </div>
+                  <button className="more-information-btn">
+                    More Information
+                  </button>
                 </div>
-                <button className="more-information-btn">
-                  More Information
-                </button>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     </>
   );
 }
